28e68ccad0d63dbfd1072961e1880d6a87075635
dotarsojat69 <tdeath457@gmail.com>
2023-10-20 19:00:41 +0700

    feat: add challenge part 2

--- 12-TypeScript-Challenge-2/problem3.ts
+++ 12-TypeScript-Challenge-2/problem3.ts
@@ -1,7 +1,27 @@
 function makeDiamond(char: string): void {
-  // your code here
-  
+  const diamondSize = char.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+    let diamond = '';
+
+    for (let i = 0; i < diamondSize; i++) {
+        diamond += ' '.repeat(diamondSize - i - 1);
+        diamond += String.fromCharCode('A'.charCodeAt(0) + i);
+        diamond += ' '.repeat(i * 2 + 1);
+        diamond += String.fromCharCode('A'.charCodeAt(0) + i);
+        diamond += '\n';
+    }
+
+    for (let i = diamondSize - 2; i >= 0; i--) {
+        diamond += ' '.repeat(diamondSize - i - 1);
+        diamond += String.fromCharCode('A'.charCodeAt(0) + i);
+        diamond += ' '.repeat(i * 2 + 1);
+        diamond += String.fromCharCode('A'.charCodeAt(0) + i);
+        diamond += '\n';
+    }
+
+    console.log(diamond);
 }
+
+
 
 makeDiamond("C");
 makeDiamond("E");
