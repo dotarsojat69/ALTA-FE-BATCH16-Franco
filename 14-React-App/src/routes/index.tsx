5c29b1146f4527d8e6f021736c4adb2e746a72a8
dotarsojat69 <tdeath457@gmail.com>
2023-12-04 01:30:12 +0700

    feat: upload deploy

--- 14-React-App/src/routes/index.tsx
+++ 14-React-App/src/routes/index.tsx
@@ -1,32 +1,68 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import ProtectedRoutes from "./protected-routes";
-import  Home from "@/pages";
+import HomePage from "@/pages";
+import NotFoundPage from "@/pages/404";
 import LoginPage from "@/pages/auth/login";
 import RegisterPage from "@/pages/auth/register";
-import DetailBookPage from "@/pages/books/detail";
-import ProfilePage from "@/pages/profile";
+import ProfilePage from "@/pages/profile/index";
 import EditProfilePage from "@/pages/profile/edit-profile";
-import HistoryBorrowPage from "@/pages/profile/history-borrow";
+import BooksPage from "@/pages/books";
+import DetailBookPage from "@/pages/books/detail";
+import HistoryPage from "@/pages/profile/history-borrow";
+import AdminPage from "@/pages/admin";
 
-export default function Router() {
-    const router = createBrowserRouter([
+import ProtectedRoute from "./protected-routes";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      element: <ProtectedRoute />,
+      children: [
         {
-            element: <ProtectedRoutes />,
-            children: [
-                {path: "/", element: <Home/>},
-                {path: "/login", element: <LoginPage />},
-                {path: "/register", element: <RegisterPage />},
-                {path: "/book/:id", element: <DetailBookPage />},
-                {path: "/profile", element: <ProfilePage />},
-                {path: "/edit-profile", element: <EditProfilePage />},
-                {path: "/history-borrow", element: <HistoryBorrowPage/>},
-                {path: "*", element: <div>404 page not found</div>
-            },
-
-            ],
+          path: "/",
+          element: <HomePage />,
         },
-        
-    ]);
-    return <RouterProvider router={router} />;
-}
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/register",
+          element: <RegisterPage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+        {
+          path: "/edit-profile",
+          element: <EditProfilePage />,
+        },
+        {
+          path: "/books",
+          element: <BooksPage />,
+        },
+        {
+          path: "/books/:id_book",
+          element: <DetailBookPage />,
+        },
+        {
+          path: "/history-borrow",
+          element: <HistoryPage />,
+        },
+        {
+          path: "/dashboard",
+          element: <AdminPage />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
